7e69aa68b1fc6557fc9641b77451dc6e4b7488e0
Lia <liaswapna@gmail.com>
2018-10-19 09:53:26 -0700

    update2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,46 +4,80 @@ var underscoreArray = [];
 var underscoreDiv = document.getElementById("underscore-div");
 var winDiv = document.getElementById("win-div");
 var guessremainDiv = document.getElementById("guessremain-div");
+var guessedDiv = document.getElementById("guessed-div");
 var rightWord = [];
 var wrongWord = [];
+var togetherWord = [];
+var CompGuess;
+var userGuess;
 var win = 0;
 var guess = 12;
-// choose word randomly
-var CompGuess = word[Math.floor(Math.random()*word.length)];
-console.log(CompGuess);
-// create underscore based on length of word
-function underscoreMyfunction() {
-    for(var i=0;i<CompGuess.length;i++){
-        underscoreArray.push("_");
-        //underscoreDiv[0].innerHTML = underscoreArray.join(" ");
+var posArray = [];
+var n =0
+function myReset(){
+    underscoreArray = [];
+    rightWord = [];
+    wrongWord = [];
+    togetherWord = [];
+    // choose word randomly
+    userGuess = "";
+    CompGuess = word[Math.floor(Math.random()*word.length)];
+    console.log(CompGuess);
+    // create underscore based on length of word
+    function underscoreMyfunction() {
+        for(var i=0;i<CompGuess.length;i++){
+            underscoreArray.push("_");
+        }
+        return underscoreArray;
     }
-    return underscoreArray;
+    underscoreDiv.innerHTML = underscoreMyfunction().join(" ");
+    guess = 12;
+    guessremainDiv.innerHTML = guess;   
+    
 }
-underscoreDiv.innerHTML = underscoreMyfunction().join(" ");
-guessremainDiv.innerHTML = guess;
-// var underscoreDiv = document.getElementById("underscore-div");
-// underscoreDiv.innerHTML = underscoreMyfunction().join(" ");
-// get usersguess
-document.onkeyup = function (event) {
-    guessremainDiv.innerHTML = guess--;
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log(userGuess);   
-    if(CompGuess.indexOf(userGuess) > -1){
-        rightWord.push(userGuess);
-        underscoreArray[CompGuess.indexOf(userGuess)] = userGuess;
-        var str = "Ana has apples!";
-// var pos1 = str.indexOf(" ");           // 3
-// var pos2 = str.indexOf(" ", pos1 + 1); // 7
-// console.log(pos2 - pos1 - 1);          // 3... the result you were expecting
-        console.log(underscoreArray);
-        underscoreDiv.innerHTML = underscoreArray.join(" ");
-        if(underscoreArray.join("") === CompGuess){
-            alert("you win");
-            win++;
-            //winDiv.textContent(win);
-            winDiv.innerHTML = win;
+myReset();
+document.onkeypress = function (event) {
+    userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    //console.log("guess_before"+guess);
+    guess--;
+    guessremainDiv.innerHTML = guess;
+    //console.log("guess_after"+guess);
+    if(guess === 0){
+        console.log("guess"+guess);
+        myReset();
+    }
+    console.log(userGuess); 
+    posArray = [];
+    for(var j=0;j<CompGuess.length;j++){
+        if(CompGuess.indexOf(userGuess,j)>-1){
+            posArray[n++] = CompGuess.indexOf(userGuess,j);
         }
     }
+    for(var r=0;r<posArray.length;r++){
+        console.log("posArray"+posArray[r]);
+    }
+    // var posLetter =  CompGuess.indexOf(userGuess);
+    // console.log(posLetter);
+    if(CompGuess.indexOf(userGuess) > -1){
+        rightWord.push(userGuess);
+        for(var k=0;k<posArray.length;k++){
+            underscoreArray[posArray[k]] = userGuess;
+            // console.log("pos: "+posArray[k]);
+            // console.log("pos: "+underscoreArray[posArray[k]]);
+        }
+        underscoreDiv.innerHTML = underscoreArray.join(" ");
+        if(underscoreArray.join("") === CompGuess){
+            //alert("you win");
+            win++;
+            winDiv.innerHTML = win;
+            myReset();
+        }
+    }else {
+        wrongWord.push(userGuess);
+    }
+    togetherWord = wrongWord.concat(rightWord);
+    console.log(togetherWord.join(","));
+    guessedDiv.innerHTML = togetherWord.join(",");
     
     
 }
