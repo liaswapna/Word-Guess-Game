0b80fa03e60691027fad04cc0df00be1934328bb
Lia <liaswapna@gmail.com>
2018-10-20 00:13:08 -0700

    updated4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,9 +17,12 @@ var underscoreDiv = document.getElementById("underscore-div");
 var winDiv = document.getElementById("win-div");
 var guessremainDiv = document.getElementById("guessremain-div");
 var guessedDiv = document.getElementById("guessed-div");
+var audioDiv = document.getElementById("myAudio"); 
+wordimageDiv.setAttribute("src","assets/images/nature.jpg");
 var rightWord = [];
 var wrongWord = [];
 var togetherWord = [];
+var togetherWordUpper = [];
 var CompGuess;
 var userGuess;
 var win = 0;
@@ -27,15 +30,28 @@ var guess = 12;
 var posArray = [];
 var n =0
 var checkReset;
+
+// Function to play and pause audio
+function playAudio() { 
+    audioDiv.play(); 
+} 
+function pauseAudio() { 
+    audioDiv.pause(); 
+}  
+
+// Reset function
 function myReset(){
     underscoreArray = [];
     rightWord = [];
     wrongWord = [];
     togetherWord = [];
-    // choose word randomly
+
+    // Choose word randomly
     userGuess = "";
     CompGuess = word[Math.floor(Math.random()*word.length)];
-    // create underscore based on length of word
+
+    // Create underscore based on length of word
+    console.log(CompGuess);
     function underscoreMyfunction() {
         for(var i=0;i<CompGuess.length;i++){
             underscoreArray.push("_");
@@ -48,38 +64,52 @@ function myReset(){
     check = 1;
 }
 myReset();
+
+// onkeypress event
 document.onkeypress = function (event) {
     check = 0;
+
+    // Get the user guess
     userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    // Proceed only if the letter is not already guessed
     if(togetherWord.indexOf(userGuess) === -1){
     guess--;
     guessremainDiv.innerHTML = guess;
     if(guess === 0){
         myReset();
     }
+
+    // Check if letter is present multiple times 
+    // Store the position in an array
     posArray = [];
     for(var j=0;j<CompGuess.length;j++){
         if(CompGuess.indexOf(userGuess,j)>-1){
             posArray[n++] = CompGuess.indexOf(userGuess,j);
         }
     }
+
+    // Proceed if the letter guess is present in computer guessed word
     if(CompGuess.indexOf(userGuess) > -1){
         rightWord.push(userGuess);
+
+        // Push the letters guessed in corresponding position
+        // HINT: By replacing the underscore
         for(var k=0;k<posArray.length;k++){
             underscoreArray[posArray[k]] = userGuess;
         }
         underscoreDiv.innerHTML = underscoreArray.join(" ");
+
+        // Proceed if userguess is equal to computer guess
         if(underscoreArray.join("") === CompGuess){
-            //get the value to Winword
             Winword = CompGuess;
-            //get the index value from word array to indexWinword
+
+            //Display the image to img-column
             indexWinword = word.indexOf(Winword);
-            //use the indexWinword to get image address from image array
             imageAddress = WordImage[indexWinword];
-            console.log(imageAddress);
-            //set attributes : display the image to img-column
             wordimageDiv.setAttribute("src",imageAddress);
-            //display label
+
+            //Display label
             labelDiv.innerHTML = Winword;
             win++;
             winDiv.innerHTML = win;
@@ -88,8 +118,15 @@ document.onkeypress = function (event) {
     }else {
         wrongWord.push(userGuess);
     }
+
+    // Display guessed letters in upper case using new array
     togetherWord = wrongWord.concat(rightWord);
-    guessedDiv.innerHTML = togetherWord.join(",");
+    for(var m = 0; m < togetherWord.length; m++){
+        togetherWordUpper[m] = togetherWord[m].toUpperCase();
+    }
+    
+    guessedDiv.innerHTML = togetherWordUpper.join(",");
+    
     if(check === 1){
         myReset();
         guessedDiv.innerHTML = "NONE";
